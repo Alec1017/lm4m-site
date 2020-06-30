d0de47c848b7c70f38579d781ffef5b4653b0222
Alec DiFederico <alecdifederico@gmail.com>
2020-06-30 14:31:15 -0400

    added red label to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,7 @@ import React from "react"
 import { getImageUrl } from "takeshape-routing"
 
 import Inner from "./inner"
+import Highlight from "./highlight"
 
 
 const Hero = ({ tagline, imagePath }) => {
@@ -12,7 +13,13 @@ const Hero = ({ tagline, imagePath }) => {
       </div>
       <Inner>
         <div className="hero__content"> 
-          <div className="hero__title">{tagline}</div>
+          <div className="hero__title">
+            {tagline.split(' ').slice(0, -1).join(' ')}
+            &nbsp;
+            <Highlight type="red">
+              {tagline.split(' ').slice(-1)[0]}
+            </Highlight>
+          </div>
         </div>
       </Inner>
     </div>
