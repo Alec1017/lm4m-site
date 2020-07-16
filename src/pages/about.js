27836d6236921514206d85a4ff7f52224770b7ad
Alec DiFederico <alecdifederico@gmail.com>
2020-07-16 17:21:20 -0400

    started work on about us section

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,83 @@
-// history
-// core values
-// motivations
-// capabilities
-
 import React from "react"
+import { getImageUrl } from "takeshape-routing"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Inner from "../components/inner"
+import Footer from "../components/footer"
+import Divider from "../components/divider"
 
-const AboutPage = ({ data, path }) => (
-  <Layout path={path}>
-    <SEO title="About" />
-    <h1>About Literature Now</h1>
-  </Layout>
-)
+const AboutPage = ({ data, path }) => {
+  const aboutDataIntro = data.takeshape.getAbout.introductionSection
+
+  return (
+    <Layout path={path}>
+      <SEO title="About" />
+      
+      <div className="introduction">
+        <div className="introduction__wrapper">
+          <Inner>
+            <div className="introduction__content">
+              <div className="introduction__intro">
+                <div className="introduction__intro__title" dangerouslySetInnerHTML={{ __html: aboutDataIntro.introduction.title.blocks[0].text }} />
+                {aboutDataIntro.introduction.text.blocks.map((value, index) => {
+                  return <div className="introduction__intro__text" key={index} dangerouslySetInnerHTML={{ __html: value.text }} />
+                })}
+                <div className="introduction__intro__image">
+                  <img src={getImageUrl(aboutDataIntro.introduction.image.path)} alt=""/>
+                </div>
+              </div>
+            </div>
+          </Inner>
+        </div>
+      </div>
+
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    takeshape {
+      getAbout {
+        introductionSection {
+          introduction {
+            image {
+              path
+            }
+            text
+            title
+          }
+          values {
+            icons {
+              image {
+                path
+              }
+            }
+            image {
+              path
+            }
+            text
+            title
+          }
+          vision {
+            image {
+              path
+            }
+            text
+            title
+          }
+          workTogether {
+            image {
+              path
+            }
+            text
+            title
+          }
+        }
+      }
+    }
+  }
+`
 
 export default AboutPage
